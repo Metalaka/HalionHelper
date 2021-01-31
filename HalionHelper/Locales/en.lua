13bf2e9ca8baa035bf91e1e34ef011b02d6c713f
Metalaka <matthieu.de.laubrie@gmail.com>
2021-02-01 00:57:09 +0100

    Fix locales

--- HalionHelper/Locales/en.lua
+++ HalionHelper/Locales/en.lua
@@ -14,4 +14,4 @@ L["ChatCommand_movableMod"] = "Movable mode enabled. Retype the command to save 
 L["ChatCommand_texture_LSM"] = "LibSharedMedia not available."
 L["ChatCommand_texture_OK"] = "Texture saved! It will be visible after a reload."
 L["ChatCommand_texture_ERROR"] = "%s is not a valid texture!"
-L["ChatCommand_cutter"] = "Cutter frame will be shown: %s"
+L["ChatCommand_cutter_message"] = "Cutter frame will be shown: %s"
--- HalionHelper/Locales/fr.lua
+++ HalionHelper/Locales/fr.lua
@@ -1,8 +1,8 @@
 if GetLocale() ~= "frFR" then return end
 
-local L = LibStub("AceLocale-3.0"):NewLocale("HalionHelper", "frFR", true)
+local L = LibStub("AceLocale-3.0"):NewLocale("HalionHelper", "frFR")
 
 L["ZoneName"] = "Le sanctum Rubis"
-L["Phase2"] = "Vous ne trouverez que souffrance au royaume du crépuscule ! Entrez si vous l'osez !"
+L["Phase2"] = "Vous ne trouverez que souffrance au royaume du Crépuscule ! Entrez si vous l'osez !"
 L["Phase3"] = "Je suis la lumière et l'ombre ! Tremblez, mortels, devant le héraut d'Aile-de-mort !"
 L["twilightcutter"] = "Les sphères volantes rayonnent d'énergie noire !"
